01c88787ce02f9b123e78983dac759966f4ddb6e
Daniel Tan <danieltan380@gmail.com>
2023-11-02 14:13:54 -0700

    Mapped over sampleData to display JSX component for each topic

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -20,10 +21,17 @@ const sampleDataForTopicList = [
   },
 ];
 
+const listItems = sampleDataForTopicList.map((topic) => {return (
+  <TopicListItem
+    key={topic.id}
+    slug={topic.slug}
+    title={topic.title} 
+  />)})
+
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {listItems}
     </div>
   );
 };
