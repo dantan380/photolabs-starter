cb6ebd40916d76e6c55974e5a781b069baf23dab
Daniel Tan <danieltan380@gmail.com>
2023-11-01 18:31:40 -0700

    Implemented fav button with useState and onClick

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,12 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [like, setLike] = useState(false);
+  const liked = () => {setLike(!like)};
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={liked}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={like}/>
       </div>
     </div>
   );
