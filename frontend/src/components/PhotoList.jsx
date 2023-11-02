97bc62e4777af7f13fdf8243eee4f7301e798f11
Daniel Tan <danieltan380@gmail.com>
2023-11-02 16:24:15 -0700

    Used mock data in photos.js and topics.js

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,66 +1,9 @@
 import React from "react";
-
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
-
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
-
-//const [ photo1, photo2, photo3 ] = sampleDataForPhotoList;
-  //const { location, urls, user, id } = photo1;
+import photos from "mocks/photos";
   
-const listItems = sampleDataForPhotoList.map((photo) => { return (<PhotoListItem
+const listItems = photos.map((photo) => { return (<PhotoListItem
   key={photo.id}
   city={photo.location.city}
   country={photo.location.country}
