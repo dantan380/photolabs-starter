38bbb31cb1ebf7d39478d21dc6a3ca718d5ec0cb
Daniel Tan <danieltan380@gmail.com>
2023-11-01 18:31:17 -0700

    Added style classes and added PhotoFavButton

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-  const {imageSource, profile, username, city, country, id} = props;
+  const { imageSource, profile, username, city, country, id } = props;
   return (
-    <>
-      <img src={imageSource} key={id}/>
-      <img src={profile}/>
-      <p>{username}</p>
-      <p>{city} {country}</p>
-    </>
-  )
+    <section className="photo-list__item">
+      <PhotoFavButton />
+      <img src={imageSource} key={id} className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={profile} className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <div className="photo-list__user-location">
+            <p>
+              {city}, {country}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PhotoListItem;
