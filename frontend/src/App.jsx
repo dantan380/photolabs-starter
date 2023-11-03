4f451a0c8524cf9222ef6edd8069b17fa3c60ca7
Daniel Tan <danieltan380@gmail.com>
2023-11-02 18:25:22 -0700

    Passed down favorites and setFavorites from useState as props. Implemented toggleFavorite function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
  const App = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = (id) => {
+    if (favorites.includes(id)) {
+      return setFavorites([...favorites.filter((photoId) => photoId !== id)]);
+    }
+    setFavorites([...favorites, id])
+  };
+
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute favorites={favorites} toggleFavorite={toggleFavorite}/>
     </div>
   );
 };
