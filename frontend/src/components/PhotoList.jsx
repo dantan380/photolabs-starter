95b881955f023e142c8bf83a315fc3178e866219
Daniel Tan <danieltan380@gmail.com>
2023-11-02 18:54:07 -0700

    Added favorites and toggleFavorite as props to pass down to PhotoListItem

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 import photos from "mocks/photos";
-  
-const listItems = photos.map((photo) => { return (<PhotoListItem
-  key={photo.id}
-  city={photo.location.city}
-  country={photo.location.country}
-  fullPhoto={photo.urls.full}
-  regularPhoto={photo.urls.regular}
-  userId={photo.user.id}
-  username={photo.user.username}
-  name={photo.user.name}
-  profileImg={photo.user.profile}
-  />) })
 
-const PhotoList = () => {
+const PhotoList = ({ favorites, toggleFavorite }) => {
   return (
     <ul className="photo-list">
-      {listItems}
+      {photos.map((photo) => {
+        return (
+          <PhotoListItem
+            key={photo.id}
+            id={photo.id}
+            city={photo.location.city}
+            country={photo.location.country}
+            fullPhoto={photo.urls.full}
+            regularPhoto={photo.urls.regular}
+            userId={photo.user.id}
+            username={photo.user.username}
+            name={photo.user.name}
+            profileImg={photo.user.profile}
+            isFavorite={favorites.includes(photo.id)} //Value of isFavorite is a boolean.
+            toggleFavorite={toggleFavorite}
+          />
+        );
+      })}
     </ul>
   );
 };
