09de43200a8ae09590652cfc712b50b7941c52e5
Daniel Tan <danieltan380@gmail.com>
2023-11-02 18:53:29 -0700

    Added TopNavigation and PhotoList. Passed down favorites and toggleFavorite as props

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import TopNavigation from 'components/TopNavigationBar';
-import PhotoList from 'components/PhotoList';
-import '../styles/HomeRoute.scss';
+import React from "react";
+import TopNavigation from "components/TopNavigationBar";
+import PhotoList from "components/PhotoList";
+import "../styles/HomeRoute.scss";
 
-const HomeRoute = () => {
+const HomeRoute = ({ favorites, toggleFavorite }) => {
   return (
     <div className="home-route">
-      <TopNavigation />
-      <PhotoList />
+
+      {/*Setting the value of isFavPhotoExist to be a boolean.*/}
+      <TopNavigation isFavPhotoExist={favorites.length > 0} />
+
+      {/*Passing down favorites and toggleFavorite as props.*/}
+      <PhotoList favorites={favorites} toggleFavorite={toggleFavorite} />
+      
     </div>
   );
 };
