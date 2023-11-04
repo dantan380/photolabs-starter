2d7a4c37c0431a6619bb8b9e6ae524bb6b59509f
Daniel Tan <danieltan380@gmail.com>
2023-11-03 18:40:51 -0700

    Linked close button to toggleModal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoListItem from 'components/PhotoListItem';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ( {toggleModal, modal} ) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={toggleModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <PhotoListItem />
     </div>
   )
 };
