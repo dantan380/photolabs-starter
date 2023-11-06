60550d70fdbdba46b9d28bc340a340029133ddfd
Daniel Tan <danieltan380@gmail.com>
2023-11-05 22:27:15 -0800

    Imported axios and useEffect. Added more switch cases for topic and photo data. Implemented useEffect for fetching photo and data topic. Called dispatch to set data.

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,11 +1,13 @@
-// import { useState } from "react";
+import { useEffect } from "react";
 import { useReducer } from "react";
+import axios from "axios";
 
 export const ACTIONS = {
   FAVORITES: "favorite",
   ADD_FAVORITE: "addFavorite",
   REMOVE_FAVORITE: "removeFavorite",
   SET_PHOTO: "setPhoto",
+  SET_PHOTO_DATA: "setPhotoData",
   SET_TOPIC_DATA: "setTopicData",
   DISPLAY_PHOTO_DETAILS: 'displayPhotoDetails'
 };
@@ -18,8 +20,10 @@ const reducer = (state, action) => {
       return {...state, favorites: state.favorites.filter((photoId) => {photoId !== action.value})};
     case ACTIONS.SET_PHOTO:
       return {...state, photo: action.value}
+    case ACTIONS.SET_PHOTO_DATA:
+      return {...state, photoData: action.payload}
     case ACTIONS.SET_TOPIC_DATA:
-      return {...state, topics: action.value}
+      return {...state, topicData: action.payload}
     case ACTIONS.DISPLAY_PHOTO_DETAILS:
       return {...state, modal: action.value}
     default:
@@ -31,14 +35,30 @@ const initialState = {
   favorites: [],
   modal: false,
   photo: null,
-  topics: []
+  topicData: [],
+  photoData: []
 };
 
 const useApplicationData = () => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  // const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const photosPromise = axios.get('http://localhost:8001/api/photos')
+    const topicsPromise = axios.get('http://localhost:8001/api/topics')
+
+    const promises = [photosPromise, topicsPromise];
+
+    Promise.all(promises)
+    .then((arrayOfPromises) => {
+      const photos = arrayOfPromises[0];
+      const topics = arrayOfPromises[1];
+
+      dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: photos.data});
+      dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: topics.data});
+    })
+  }, []);
 
   const toggleFavorite = (id) => {
     state.favorites.includes(id) ? dispatch({ type: ACTIONS.REMOVE_FAVORITE, value: id}) : dispatch({ type: ACTIONS.ADD_FAVORITE, value: id});
@@ -49,26 +69,6 @@ const useApplicationData = () => {
     state.modal === false ? dispatch({ type: ACTIONS.SET_PHOTO, value: photo}) : dispatch({ type: ACTIONS.SET_PHOTO, value: null})
   };
 
-  // //Function to check if the photo-id of the liked photo is included in the favorites array.
-  // const toggleFavorite = (id) => {
-  //   if (favorites.includes(id)) {
-  //     //If the element in favorites is equal to the given id, it produces a false boolean to remove it from favorites.
-  //     return setFavorites([...favorites.filter((photoId) => photoId !== id)]);
-  //   }
-  //   //Otherwise, add the photo to favorites.
-  //   setFavorites([...favorites, id])
-  // };
-
-  // const [modal, setModal] = useState(false);
-  // const [photo, setPhoto] = useState(null);
-
-  // const toggleModalWithPhoto = (photo) => {
-  //   setModal(!modal);
-  //   modal === false ? setPhoto(photo) : setPhoto(null)
-  // };
-
-
-  // return { favorites, modal, photo, toggleFavorite, toggleModalWithPhoto };
   return {
     state,
     toggleFavorite,
