0b58af8384c98acf1e74d4468d8a41755a0a1199
Daniel Tan <danieltan380@gmail.com>
2023-11-06 12:51:24 -0800

    Adjusted remove favorite case to implicit return. Adjusted getPhotosByTopic to call dispatch with correct type. Formatted. '

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -9,25 +9,32 @@ export const ACTIONS = {
   SET_PHOTO: "setPhoto",
   SET_PHOTO_DATA: "setPhotoData",
   SET_TOPIC_DATA: "setTopicData",
-  DISPLAY_PHOTO_DETAILS: 'displayPhotoDetails'
+  DISPLAY_PHOTO_DETAILS: "displayPhotoDetails",
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.ADD_FAVORITE:
-      return {...state, favorites: [...state.favorites, action.value]};
+      return { ...state, favorites: [...state.favorites, action.value] };
     case ACTIONS.REMOVE_FAVORITE:
-      return {...state, favorites: state.favorites.filter((photoId) => {photoId !== action.value})};
+      return {
+        ...state,
+        favorites: state.favorites.filter(
+          (photoId) => photoId !== action.value
+        ),
+      };
     case ACTIONS.SET_PHOTO:
-      return {...state, photo: action.value}
+      return { ...state, photo: action.value };
     case ACTIONS.SET_PHOTO_DATA:
-      return {...state, photoData: action.payload}
+      return { ...state, photoData: action.payload };
     case ACTIONS.SET_TOPIC_DATA:
-      return {...state, topicData: action.payload}
+      return { ...state, topicData: action.payload };
     case ACTIONS.DISPLAY_PHOTO_DETAILS:
-      return {...state, modal: action.value}
+      return { ...state, modal: action.value };
     default:
-      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
   }
 };
 
@@ -36,44 +43,63 @@ const initialState = {
   modal: false,
   photo: null,
   topicData: [],
-  photoData: []
+  photoData: [],
 };
 
 const useApplicationData = () => {
-
   const [state, dispatch] = useReducer(reducer, initialState);
 
-
+  //useEffect for any side effects (things that our function is pulling from outside itself) that happen.
   useEffect(() => {
-    const photosPromise = axios.get('http://localhost:8001/api/photos')
-    const topicsPromise = axios.get('http://localhost:8001/api/topics')
+    const photosPromise = axios.get("/api/photos");
+    const topicsPromise = axios.get("/api/topics");
 
     const promises = [photosPromise, topicsPromise];
 
     Promise.all(promises)
-    .then((arrayOfPromises) => {
-      const photos = arrayOfPromises[0];
-      const topics = arrayOfPromises[1];
+      .then((arrayOfPromises) => {
+        const photos = arrayOfPromises[0];
+        const topics = arrayOfPromises[1];
 
-      dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: photos.data});
-      dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: topics.data});
-    })
+        dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: photos.data });
+        dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: topics.data });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }, []);
 
+  const getPhotosByTopic = (topicId) => {
+    axios
+      .get(`/api/topics/photos/${topicId}`)
+      .then((res) => {
+        const photosOfTopic = res.data;
+        dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: photosOfTopic });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   const toggleFavorite = (id) => {
-    state.favorites.includes(id) ? dispatch({ type: ACTIONS.REMOVE_FAVORITE, value: id}) : dispatch({ type: ACTIONS.ADD_FAVORITE, value: id});
-  }
+    state.favorites.includes(id)
+      ? dispatch({ type: ACTIONS.REMOVE_FAVORITE, value: id })
+      : dispatch({ type: ACTIONS.ADD_FAVORITE, value: id });
+  };
 
   const toggleModalWithPhoto = (photo) => {
-    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, value: !state.modal})
-    state.modal === false ? dispatch({ type: ACTIONS.SET_PHOTO, value: photo}) : dispatch({ type: ACTIONS.SET_PHOTO, value: null})
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, value: !state.modal });
+    state.modal === false
+      ? dispatch({ type: ACTIONS.SET_PHOTO, value: photo })
+      : dispatch({ type: ACTIONS.SET_PHOTO, value: null });
   };
 
   return {
     state,
     toggleFavorite,
-    toggleModalWithPhoto
-  }
+    toggleModalWithPhoto,
+    getPhotosByTopic,
+  };
 };
 
 export default useApplicationData;
