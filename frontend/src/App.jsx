acee3029fc7cfb29d3b7ce31b664d5e5293b138b
Daniel Tan <danieltan380@gmail.com>
2023-11-05 18:30:25 -0800

    Updated to import useApplicationData. Updated props to make sure render still happens correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useApplicationData from 'hooks/useApplicationData';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
@@ -6,41 +7,23 @@ import photos from "mocks/photos";
 
 // Note: Rendering a single component to build components in isolation
  const App = () => {
-  const [favorites, setFavorites] = useState([]);
-
-  //Function to check if the photo-id of the liked photo is included in the favorites array.
-  const toggleFavorite = (id) => {
-    if (favorites.includes(id)) {
-      //If the element in favorites is equal to the given id, it produces a false boolean to remove it from favorites.
-      return setFavorites([...favorites.filter((photoId) => photoId !== id)]);
-    }
-    //Otherwise, add the photo to favorites.
-    setFavorites([...favorites, id])
-  };
-
-  const [modal, setModal] = useState(false);
-  const [photo, setPhoto] = useState(null);
-
-  const toggleModalWithPhoto = (photo) => {
-    setModal(!modal);
-    modal === false ? setPhoto(photo) : setPhoto(null)
-  };
+  const { state, toggleFavorite, toggleModalWithPhoto } = useApplicationData();
 
   return (
     <div className="App">
       <HomeRoute 
-      favorites={favorites}
+      favorites={state.favorites}
        toggleFavorite={toggleFavorite}
         toggleModalWithPhoto={toggleModalWithPhoto} 
-        modal={modal}
+        modal={state.modal}
         photos={photos}
         />
-      {modal && (
+      {state.modal && (
       <PhotoDetailsModal 
       toggleModalWithPhoto={toggleModalWithPhoto} 
-      photo={photo}
-      modal={modal} 
-      favorites={favorites}
+      photo={state.photo}
+      modal={state.modal} 
+      favorites={state.favorites}
       toggleFavorite={toggleFavorite}
       />
       )}
