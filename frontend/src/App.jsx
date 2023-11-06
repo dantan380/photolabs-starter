70bd98c8ce6a36f65d094ada3b3a21e5e3b99f0b
Daniel Tan <danieltan380@gmail.com>
2023-11-06 12:52:07 -0800

    Adjusted typo with getPhotosByTopic being passed as props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import useApplicationData from 'hooks/useApplicationData';
-import './App.scss';
-import HomeRoute from 'routes/HomeRoute';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import React from "react";
+import useApplicationData from "hooks/useApplicationData";
+import "./App.scss";
+import HomeRoute from "routes/HomeRoute";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 
 // Note: Rendering a single component to build components in isolation
- const App = () => {
-  const { state, toggleFavorite, toggleModalWithPhoto } = useApplicationData();
+const App = () => {
+  const { state, toggleFavorite, toggleModalWithPhoto, getPhotosByTopic } =
+    useApplicationData();
 
   return (
     <div className="App">
       <HomeRoute
-      topics={state.topicData}
-      favorites={state.favorites}
-       toggleFavorite={toggleFavorite}
-        toggleModalWithPhoto={toggleModalWithPhoto} 
+        getPhotosByTopic={getPhotosByTopic}
+        topics={state.topicData}
+        favorites={state.favorites}
+        toggleFavorite={toggleFavorite}
+        toggleModalWithPhoto={toggleModalWithPhoto}
         modal={state.modal}
         photos={state.photoData}
-        />
-      {state.modal && (
-      <PhotoDetailsModal 
-      toggleModalWithPhoto={toggleModalWithPhoto} 
-      photo={state.photo}
-      modal={state.modal} 
-      favorites={state.favorites}
-      toggleFavorite={toggleFavorite}
       />
+      {state.modal && (
+        <PhotoDetailsModal
+          toggleModalWithPhoto={toggleModalWithPhoto}
+          photo={state.photo}
+          modal={state.modal}
+          favorites={state.favorites}
+          toggleFavorite={toggleFavorite}
+        />
       )}
     </div>
   );
