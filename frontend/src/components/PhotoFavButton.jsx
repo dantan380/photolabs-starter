01eb3015d49699f1912e57cbc3cd7b53946fed53
Daniel Tan <danieltan380@gmail.com>
2023-11-06 12:54:27 -0800

    Formatted

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useState } from "react";
-
+import React from "react";
 import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton(props) {
   return (
-    <div className="photo-list__fav-icon" onClick={() => props.toggleFavorite(props.photoId)}>
+    <div
+      className="photo-list__fav-icon"
+      onClick={() => props.toggleFavorite(props.photoId)}
+    >
       <div className="photo-list__fav-icon-svg">
         <FavIcon selected={props.isFavorite} />
       </div>
