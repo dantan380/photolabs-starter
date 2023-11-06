5b4cf18c7971a52eb6af3880eb7ba97abbf6e2fd
Daniel Tan <danieltan380@gmail.com>
2023-11-06 12:53:15 -0800

    Adjusted typo for getPhotosByTopic prop

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,20 +3,31 @@ import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
 import "../styles/HomeRoute.scss";
 
-const HomeRoute = ({ topics, favorites, toggleFavorite, modal, toggleModalWithPhoto, photos }) => {
+const HomeRoute = ({
+  topics,
+  favorites,
+  toggleFavorite,
+  modal,
+  toggleModalWithPhoto,
+  photos,
+  getPhotosByTopic,
+}) => {
   return (
     <div className="home-route">
-
       {/*Setting the value of isFavPhotoExist to be a boolean.*/}
-      <TopNavigation topics={topics} isFavPhotoExist={favorites.length > 0} />
+      <TopNavigation
+        getPhotosByTopic={getPhotosByTopic}
+        topics={topics}
+        isFavPhotoExist={favorites.length > 0}
+      />
 
       {/*Passing down favorites and toggleFavorite as props.*/}
-      <PhotoList 
-      photos={photos}
-      favorites={favorites} 
-      toggleFavorite={toggleFavorite} 
-      modal={modal} 
-      toggleModalWithPhoto={toggleModalWithPhoto}
+      <PhotoList
+        photos={photos}
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
+        modal={modal}
+        toggleModalWithPhoto={toggleModalWithPhoto}
       />
     </div>
   );
