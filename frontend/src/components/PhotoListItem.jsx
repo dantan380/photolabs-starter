b52050861137bec1b94534978cf9abcac1122534
Daniel Tan <danieltan380@gmail.com>
2023-11-06 12:54:05 -0800

    Adjusted typo for getPhotosByTopic prop

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-
   return (
     <section className="photo-list__item">
       <PhotoFavButton
@@ -12,10 +10,17 @@ const PhotoListItem = (props) => {
         isFavorite={props.isFavorite}
         toggleFavorite={props.toggleFavorite}
       />
-      <div onClick={() => {props.toggleModalWithPhoto(props.photo)}}>
+      <div
+        onClick={() => {
+          props.toggleModalWithPhoto(props.photo);
+        }}
+      >
         <img src={props.photo.urls.regular} className="photo-list__image" />
         <div className="photo-list__user-details">
-          <img src={props.photo.user.profile} className="photo-list__user-profile" />
+          <img
+            src={props.photo.user.profile}
+            className="photo-list__user-profile"
+          />
           <div className="photo-list__user-info">
             <p>{props.photo.user.name}</p>
             <div className="photo-list__user-location">
