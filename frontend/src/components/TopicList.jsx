f281d32396e40f734c3e7338451f8b0e7f7dce6e
Daniel Tan <danieltan380@gmail.com>
2023-11-05 22:25:17 -0800

    Used topics received from axios get request.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-import topics from "mocks/topics";
 
-const listItems = topics.map((topic) => {return (
-  <TopicListItem
-    key={topic.id}
-    slug={topic.slug}
-    title={topic.title} 
-  />)})
+const TopicList = ({ topics }) => {
 
-const TopicList = () => {
+  const listItems = topics.map((topic) => {return (
+    <TopicListItem
+      key={topic.id}
+      slug={topic.slug}
+      title={topic.title} 
+    />)})
+
   return (
     <div className="top-nav-bar__topic-list">
       {listItems}
