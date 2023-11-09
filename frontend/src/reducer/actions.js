6c77ae022d1361d9fe168f354d7db42cdfbd4678
Daniel Tan <danieltan380@gmail.com>
2023-11-08 21:41:18 -0800

    Moved ACTIONS object to this seperate file to be exported

---
+++ frontend/src/reducer/actions.js
@@ -0,0 +1,9 @@
+export const ACTIONS = {
+  FAVORITES: "favorite",
+  ADD_FAVORITE: "addFavorite",
+  REMOVE_FAVORITE: "removeFavorite",
+  SET_PHOTO: "setPhoto",
+  SET_PHOTO_DATA: "setPhotoData",
+  SET_TOPIC_DATA: "setTopicData",
+  DISPLAY_PHOTO_DETAILS: "displayPhotoDetails",
+};
